56dab2a76089ac444414e1026ffda8965bfecb2d
Felix <Mcheese134@gmail.com>
2020-11-23 16:00:36 -0500

    Added back button in login

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -19,6 +19,8 @@ $(document).ready(function () {
       closeNav();
     }); 
 
+    initMap()
+
 
   });
 
@@ -30,4 +32,19 @@ function openNav() {
 function closeNav() {
   console.log("I closed");
   document.getElementById("mySidenav").style.width = "0";
+}
+
+function initMap() {
+  // The location of Uluru
+  const uluru = { lat: -25.344, lng: 131.036 };
+  // The map, centered at Uluru
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: uluru,
+  });
+  // The marker, positioned at Uluru
+  const marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
+  });
 }
