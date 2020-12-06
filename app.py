64a63d8e9fa74731e3eb270726ce21ef7b9d3127
Felix <Mcheese134@gmail.com>
2020-12-05 20:55:42 -0500

    Adding more to the modal for sign-up

--- app.py
+++ app.py
@@ -30,6 +30,20 @@ class posts(db.Model):
     def __repr__(self):
         return '<Name %r>' % self.id
 
+class users(db.Model):
+    username = db.Column(db.String(200), primary_key = True)
+    password = db.Column(db.String(200), nullable = False)
+    email = db.Column(db.String(200), nullable = False)
+    firstname = db.Column(db.String(200), nullable = False)
+    lastname = db.Column(db.String(200), nullable = False)
+    location = db.Column(db.String(200), nullable = False)
+    
+        # function to return a string when we add something
+    def __repr__(self):
+        return '<User %r>' % self.username
+    
+
+
 class NameForm(FlaskForm):
     usr = StringField('Enter an Username: ',
                              validators=[DataRequired()])
@@ -68,6 +82,37 @@ def create():
     else:
         p = posts.query.order_by(posts.id)
         return render_template('create.html', title=title, create=p)
+
+
+#Sign-up Route
+@app.route('/signup', methods = ['POST'])
+def signup():
+
+    print("I am here")
+    if request.method == "POST":
+        f = request.form['fname']
+        l = request.form['lname']
+        loc = request.form['loc']
+        u = request.form['uname']
+        p = request.form['psw']
+        e = request.form['email']
+        
+        new_user = users(username = u, password = p, email = e, firstname = f, lastname = l, location = loc)
+
+        try:
+            print("I am here")
+            db.session.add(new_user)
+            print("I am added")
+            db.session.commit()
+            print("I am Commited")
+
+            return redirect(url_for('login'))
+        except: 
+            return "There was an error creating your user"
+
+    else:
+        print("Should not reach here; there is no GET for signup")
+        return redirect(url_for('login'))
 
 @app.route('/profile')
 def profile():
